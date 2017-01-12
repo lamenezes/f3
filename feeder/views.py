9e3916510b2f4a1ff603c1837ee64a4f649e69e5
Luiz Menezes <luiz.menezesf@gmail.com>
2017-01-12 21:45:32 -0200

    Add feed view

--- feeder/views.py
+++ feeder/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+# coding: utf-8
 
-# Create your views here.
+from django.http import HttpResponse
+
+from feeder.utils import fetch_archive_xml, edit_archive_xml
+
+
+def feed(request):
+    xml = edit_archive_xml(fetch_archive_xml())
+    return HttpResponse(xml, content_type='text/xml')
