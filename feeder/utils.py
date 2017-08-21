f94a9d4ee2fa3db165d7eaf4539051af546be240
Luiz Menezes <luiz.menezesf@gmail.com>
2017-08-21 00:55:47 -0300

    Properly add namespace do itunes element on edited xml

--- feeder/utils.py
+++ feeder/utils.py
@@ -4,6 +4,10 @@ import re
 import urllib
 from xml.etree import ElementTree
 
+xml_namespaces = {
+    'itunes': 'http://www.itunes.com/dtds/podcast-1.0.dtd'
+}
+
 
 def fetch_archive_xml():
     url = 'http://archive.org/services/collection-rss.php?query=creator%3A%22Fanficast%22'
@@ -12,28 +16,31 @@ def fetch_archive_xml():
 
 
 def add_itunes_metadata(rss):
+    # TODO: change podcast image name convention to:
+    # vitrine-quadrada-fanficando-1 or vitrine-quadrada-fanficast-2 or
+    # vitrine-quadrada-virilhada-cultural-10
     base_image_url = 'http://fanficast.com.br/static/media/vitrine-quadrada-{}.png'
     rss.attrib['xmlns:itunes'] = 'http://www.itunes.com/dtds/podcast-1.0.dtd'
+    namespace_metadata = {'{%s}' % xml_namespaces['itunes']: ''}
 
     channel = rss.getchildren()[0]
     for item in channel.findall('item'):
-        itunes_explicit = ElementTree.SubElement(item, 'itunes:explicit')
+        itunes_explicit = ElementTree.SubElement(item, 'itunes:explicit', namespace_metadata)
         itunes_explicit.text = 'no'
 
-        itunes_author = ElementTree.SubElement(item, 'itunes:author')
+        itunes_author = ElementTree.SubElement(item, 'itunes:author', namespace_metadata)
         itunes_author.text = 'Fanficast'
 
         title = item.find('title')
         episode_number = re.match('.* (\d+) - .*', title.text).groups()[0].zfill(2)
 
-        itunes_image = ElementTree.SubElement(item, 'itunes:image')
+        itunes_image = ElementTree.SubElement(item, 'image', namespace_metadata)
         itunes_image.attrib['href'] = base_image_url.format(episode_number)
 
     return rss
 
 
-def edit_archive_xml(xml):
-    rss = ElementTree.fromstring(xml)
+def update_metadata(rss):
     channel = rss.getchildren()[0]
 
     link = channel.find('link')
@@ -59,6 +66,12 @@ def edit_archive_xml(xml):
     image_link = image.find('link')
     image_link.text = u'https://fanficast.com.br/'
 
+    return rss
+
+
+def edit_archive_xml(xml):
+    rss = ElementTree.fromstring(xml)
+    rss = update_metadata(rss)
     rss = add_itunes_metadata(rss)
 
     return ElementTree.tostring(rss)
